3109f680905acc49f6d47d7ba1eced22bfb25993
Myniuu <michalszczygiel96@gmail.com>
2024-04-23 20:05:26 +0200

    add logic to pass value from input to console

--- Weather app/src/App.jsx
+++ Weather app/src/App.jsx
@@ -1,10 +1,26 @@
+import { useState } from "react";
 import { Header } from "./components/Header/Header";
 import { Info } from "./components/Info/Info";
 
 export function App() {
+  const [inputValue, setInputValue] = useState("");
+
+  function handleButtonClick() {
+    console.log(inputValue);
+    setInputValue("");
+  }
+
+  function handleChange(e) {
+    setInputValue(e.target.value);
+  }
+
   return (
     <>
-      <Header />
+      <Header
+        onClick={handleButtonClick}
+        onChange={handleChange}
+        value={inputValue}
+      />
       <Info />
     </>
   );
--- Weather app/src/components/Header/Header.jsx
+++ Weather app/src/components/Header/Header.jsx
@@ -1,12 +1,17 @@
 import styles from "./Header.module.css";
 
-export function Header() {
+export function Header({ value, onClick, onChange }) {
   return (
     <div className={styles.header}>
       <h1>Weather</h1>
       <div className={styles.container}>
-        <input type="text" placeholder="Write city..." />
-        <button>Search</button>
+        <input
+          value={value}
+          onChange={onChange}
+          type="text"
+          placeholder="Write city..."
+        />
+        <button onClick={onClick}>Search</button>
       </div>
     </div>
   );

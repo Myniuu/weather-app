fdb83f8637dcc66d158f828ea4740ac796987560
Myniuu <michalszczygiel96@gmail.com>
2024-04-24 18:37:35 +0200

    added fetch from free weather api

--- Weather app/src/App.jsx
+++ Weather app/src/App.jsx
@@ -4,14 +4,27 @@ import { Info } from "./components/Info/Info";
 
 export function App() {
   const [inputValue, setInputValue] = useState("");
+  const [data, setData] = useState([]);
 
   function handleButtonClick() {
-    console.log(inputValue);
+    callWeather(inputValue);
     setInputValue("");
   }
 
   function handleChange(e) {
     setInputValue(e.target.value);
+  }
+
+  function callWeather(city) {
+    fetch(
+      `http://api.weatherapi.com/v1/current.json?key=2dd80bf3d57945faa57154500242404&q=${city}&aqi=no`
+    )
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        }
+      })
+      .then((res) => console.log(res));
   }
 
   return (
@@ -21,7 +34,7 @@ export function App() {
         onChange={handleChange}
         value={inputValue}
       />
-      <Info />
+      <Info city={data} />
     </>
   );
 }
--- Weather app/src/components/Info/Info.jsx
+++ Weather app/src/components/Info/Info.jsx
@@ -1,13 +1,13 @@
 import styles from "./Info.module.css";
 
-export function Info() {
+export function Info({ city, temperature, wind, clouds }) {
   return (
     <div className={styles.container}>
-      <h2>Warsaw</h2>
+      <h2>{city}</h2>
       <h3>broken clouds</h3>
-      <h3>9 celcius</h3>
-      <h3>4.47 km/h</h3>
-      <h3>72 %</h3>
+      <h3>{temperature} °C</h3>
+      <h3>{wind} km/h</h3>
+      <h3>{clouds} %</h3>
     </div>
   );
 }
